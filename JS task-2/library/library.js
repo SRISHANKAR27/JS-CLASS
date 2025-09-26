a5d82ab38cadf0fe99fbf4c03f8025cf76ae86cc
SRISHANKAR27 <sri2700339@gmail.com>
2025-09-26 14:28:58 +0530

    1

--- JS task-2/library/library.js
+++ JS task-2/library/library.js
@@ -1,20 +1,13 @@
 function librarySystem() {
-    // Step 1: Ask for username and password
     let username = prompt("Enter username:");
     let password = prompt("Enter password:");
 
-    if (username === "library" && password === "book456") {
-        // Successful login
+    if (username === "library" && password === "27072003") {
         let borrow = confirm("Do you want to borrow a book?");
 
         if (borrow) {
-            // Step 3: Ask for book category
             let category = prompt("Enter book category:\n1 = Fiction\n2 = Science\n3 = History");
-
-            // Convert to number
             category = Number(category);
-
-            // Step 4: Use switch
             switch (category) {
                 case 1:
                     alert("You selected Fiction");
@@ -29,14 +22,11 @@ function librarySystem() {
                     alert("Invalid category!");
             }
         } else {
-            // Step 5: User clicked Cancel
+           
             alert("Maybe next time! Goodbye!");
         }
     } else {
-        // Invalid login
         alert("Invalid login!");
     }
 }
-
-// Run the function
 librarySystem();
